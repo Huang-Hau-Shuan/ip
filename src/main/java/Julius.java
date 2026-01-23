63785241fd27fa9fa36ddaf24930658313f24ffb
hau shuan <huanghaushuan@gmail.com>
2026-01-23 16:53:35 +0800

    Rename chatbot to Julius

---
+++ src/main/java/Julius.java
@@ -0,0 +1,12 @@
+public class Julius {
+    public static void main(String[] args) {
+        String logo = "      _ _    _ _      _____ _    _  _____\n"
+                + "     | | |  | | |    |_   _| |  | |/ ____|\n"
+                + "     | | |  | | |      | | | |  | | (___  \n"
+                + " _   | | |  | | |      | | | |  | |\\___ \\ \n"
+                + "| |__| | |__| | |____ _| |_| |__| |____) |\n"
+                + " \\____/ \\____/|______|_____|\\____/|_____/ \n";
+
+        System.out.println("Hello from\n" + logo);
+    }
+}
