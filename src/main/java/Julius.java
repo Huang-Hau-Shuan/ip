e8b6addc6d97d03ca4e6d9fbdd3d0df109431b5a
hau shuan <huanghaushuan@gmail.com>
2026-01-28 23:47:00 +0800

    Add the ability to store whatever text entered by the user and display them back to the user when requested

--- src/main/java/Julius.java
+++ src/main/java/Julius.java
@@ -3,6 +3,11 @@ import java.util.Scanner;
 public class Julius {
     public static void main(String[] args) {
         String divider = "____________________________________________________________";
+
+        // Level-2: Initialise storage array and a counter
+        String[] tasks = new String[100];
+        int taskCount = 0;
+
         String logo = "      _ _    _ _      _____ _    _  _____\n"
                 + "     | | |  | | |    |_   _| |  | |/ ____|\n"
                 + "     | | |  | | |      | | | |  | | (___  \n"
@@ -30,7 +35,33 @@ public class Julius {
             }
 
             System.out.println(divider);
-            System.out.println("    " + input); // Echo the input
+
+            if (input.equalsIgnoreCase("list")){
+                // Level-2: List all tasks
+                if (taskCount == 0) {
+                    System.out.println("    No tasks in your list.");
+                } else {
+                    // System.out.println("    Here are the tasks in your list:");
+                    for (int i = 0; i < taskCount; i++) {
+                        System.out.println("    " + (i + 1) + ". " + tasks[i]);
+                    }
+                }
+            } else if (processedInput.startsWith("add ")) {
+                // Level-2: Add a new task
+                String task = input.substring(4).trim();
+                if (!task.isEmpty()) {
+                    tasks[taskCount] = task;
+                    taskCount++;
+                    System.out.println("    Added task: " + task);
+                } else {
+                    System.out.println("    Please provide a task to add.");
+                }
+            } else {
+                // System.out.println("    I'm sorry, I don't understand that command:");
+                System.out.println("    " + input); // Echo the input
+            }
+
+
             System.out.println(divider);
         }
 
