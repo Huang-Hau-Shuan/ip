0dc67087006191360f6b99c68d965ad097312672
hau shuan <huanghaushuan@gmail.com>
2026-01-28 22:00:27 +0800

    Echo user input

--- src/main/java/Julius.java
+++ src/main/java/Julius.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Julius {
     public static void main(String[] args) {
         String logo = "      _ _    _ _      _____ _    _  _____\n"
@@ -15,6 +17,17 @@ public class Julius {
         System.out.println(" Hello! I'm " + name);
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
+
+        Scanner in = new Scanner(System.in);
+        String line;
+
+        while (true) {
+            line = in.nextLine();
+            if (line.equals("bye")) {
+                break;
+            }
+            System.out.println(line); // Echo the input
+        }
 
         // Exit message
         System.out.println(" Bye. Hope to see you again soon!");
