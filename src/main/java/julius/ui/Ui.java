d51eb622ca473583f6a7a6a26befefdf61a023cb
hau shuan <huanghaushuan@gmail.com>
2026-02-28 15:19:33 +0800

    Add the output methods Parser needs

--- src/main/java/julius/ui/Ui.java
+++ src/main/java/julius/ui/Ui.java
@@ -1,5 +1,8 @@
 package julius.ui;
 
+import julius.task.Task;
+import julius.task.TaskList;
+
 /**
  * Deals with interactions with the user.
  * Handles all output printed to the console.
@@ -34,5 +37,48 @@ public class Ui {
     /** Prints the horizontal divider line. */
     public void showDivider() {
         System.out.println(DIVIDER);
+    }
+
+    /** Prints an error message. */
+    public void showError(String message) {
+        System.out.println("    " + message);
+    }
+
+    /** Prints all tasks in the list. */
+    public void showTaskList(TaskList tasks) {
+        if (tasks.isEmpty()) {
+            System.out.println("    No tasks in your list.");
+            return;
+        }
+        System.out.println(" Here are the tasks in your list:");
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println(" " + (i + 1) + "." + tasks.get(i).toString());
+        }
+    }
+
+    /** Prints the confirmation that a task was added. */
+    public void showTaskAdded(Task task, int totalTasks) {
+        System.out.println(" Got it. I've added this task:");
+        System.out.println("   " + task.toString());
+        System.out.println(" Now you have " + totalTasks + " tasks in the list.");
+    }
+
+    /** Prints the confirmation that a task was deleted. */
+    public void showTaskDeleted(Task task, int totalTasks) {
+        System.out.println(" Noted. I've removed this task:");
+        System.out.println("   " + task.toString());
+        System.out.println(" Now you have " + totalTasks + " tasks in the list.");
+    }
+
+    /** Prints the confirmation that a task was marked done. */
+    public void showTaskMarkedDone(Task task) {
+        System.out.println(" Nice! I've marked this task as done:");
+        System.out.println("   " + task.toString());
+    }
+
+    /** Prints the confirmation that a task was marked not done. */
+    public void showTaskMarkedNotDone(Task task) {
+        System.out.println(" OK, I've marked this task as not done yet:");
+        System.out.println("   " + task.toString());
     }
 }
