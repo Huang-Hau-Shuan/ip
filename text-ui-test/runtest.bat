791d06a518ed95f37d2d885f30b1d7406d85f8b7
hau shuan <huanghaushuan@gmail.com>
2026-02-28 15:54:22 +0800

    Fix test script so that it runs an empty data file to compare outputs of Expected and Actual

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -6,16 +6,19 @@ if not exist ..\bin mkdir ..\bin
 REM delete output from previous run
 if exist ACTUAL.TXT del ACTUAL.TXT
 
+REM delete test data file from previous run (separate from real user data)
+if exist ..\data\julius-test.txt del ..\data\julius-test.txt
+
 REM compile the code into the bin folder
-javac  -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\julius\*.java ..\src\main\java\julius\task\*.java
+javac  -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\julius\*.java ..\src\main\java\julius\command\*.java ..\src\main\java\julius\exception\*.java ..\src\main\java\julius\parser\*.java ..\src\main\java\julius\storage\*.java ..\src\main\java\julius\task\*.java ..\src\main\java\julius\ui\*.java
 IF ERRORLEVEL 1 (
     echo ********** BUILD FAILURE **********
     exit /b 1
 )
 REM no error here, errorlevel == 0
 
-REM run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ..\bin julius.Julius < input.txt > ACTUAL.TXT
+REM run the program with a dedicated test data file so real user data is untouched
+java -classpath ..\bin julius.Julius ..\data\julius-test.txt < input.txt > ACTUAL.TXT
 
 REM compare the output to the expected output
 FC ACTUAL.TXT EXPECTED.TXT
--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -12,15 +12,21 @@ then
     rm ACTUAL.TXT
 fi
 
+# delete test data file from previous run (separate from real user data)
+if [ -e "../data/julius-test.txt" ]
+then
+    rm ../data/julius-test.txt
+fi
+
 # compile the code into the bin folder, terminates if error occurred
-if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/julius/*.java ../src/main/java/julius/task/*.java
+if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/julius/*.java ../src/main/java/julius/command/*.java ../src/main/java/julius/exception/*.java ../src/main/java/julius/parser/*.java ../src/main/java/julius/storage/*.java ../src/main/java/julius/task/*.java ../src/main/java/julius/ui/*.java
 then
     echo "********** BUILD FAILURE **********"
     exit 1
 fi
 
-# run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ../bin julius.Julius < input.txt > ACTUAL.TXT
+# run the program with a dedicated test data file so real user data is untouched
+java -classpath ../bin julius.Julius ../data/julius-test.txt < input.txt > ACTUAL.TXT
 
 # convert to UNIX format
 cp EXPECTED.TXT EXPECTED-UNIX.TXT
